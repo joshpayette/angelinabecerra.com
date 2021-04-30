3bce09e196fff8694fca917bff4e0c0537d1f6bb
Josh Payette <hello@joshpayette.dev>
2021-04-30 10:43:06 -0400

    Added additional build paths

--- pages/gallery/[folderName].tsx
+++ pages/gallery/[folderName].tsx
@@ -5,7 +5,14 @@ import { Gallery } from 'components/Gallery'
 
 export async function getStaticPaths() {
   return {
-    paths: [{ params: { folderName: 'event-photography' } }],
+    paths: [
+      { params: { folderName: 'event-photography' } },
+      { params: { folderName: 'fine-art' } },
+      { params: { folderName: 'photo-275-portfolio' } },
+      { params: { folderName: 'portrait-and-fashion' } },
+      { params: { folderName: 'pride-and-hope' } },
+      { params: { folderName: 'protest-and-unrest' } },
+    ],
     fallback: false,
   }
 }
